c38cb8aaf72dea0eb9ff7772762c5110d1a1a680
pperezp <patodeath@gmail.com>
2018-04-16 00:03:13 -0300

    Colocando los links de notifications

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Experience</title>
+        <!-- 
+            https://www.cssscript.com/demo/minimal-notification-popup-pure-javascript/
+            https://www.cssscript.com/minimal-notification-popup-pure-javascript/
+         -->
         <link rel="stylesheet" href="css/notifications.css">
         <script src="js/notifications.js"></script>
 
