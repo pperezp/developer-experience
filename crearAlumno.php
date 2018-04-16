cd45b3834abbd069a001d69c480481c56aea1a41
pperezp <patodeath@gmail.com>
2018-04-16 00:33:35 -0300

    Se deja la creación de alumnos con ajax

--- crearAlumno.php
+++ crearAlumno.php
@@ -5,12 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+
+        <!-- Bootstrap y jquery-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Bootstrap y jquery-->
+
+        <script>
+        $(document).ready(function(){
+            $("#nombre").keyup(function(event){
+                if(event.which == 13){ // si es enter
+                    $.ajax({
+                        type: 'POST',
+                        url: 'http://localhost/experience/controller/crearAlumno.php',
+                        data: {
+                            nombre: $("#nombre").val()
+                        }
+                    }).done(function (res) {
+                        $("#nombre").val("")
+                        $("#nombre").focus();
+                    });
+                }
+            });
+        });
+        </script>
     </head>
     <body>
-        <form action="controller/crearAlumno.php" method="post">
-            <input type="text" name="nombre" placeholder="Nombre alumno:">
-            <input type="submit" value="Crear alumno">
-        </form>
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre alumno:">
         <a href="index.php">Volver</a>
     </body>
 </html>
